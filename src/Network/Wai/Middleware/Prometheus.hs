e17c443278e47800f8bbb37078280d32ed253a0e
BurningWitness <frozenwitness@gmail.com>
2022-08-08 13:00:49 +0700

    Simplifying HTTP metrics

--- src/Network/Wai/Middleware/Prometheus.hs
+++ src/Network/Wai/Middleware/Prometheus.hs
@@ -7,6 +7,7 @@ module Network.Wai.Middleware.Prometheus where
 import           Prometheus
 import           Type.No
 
+import           Data.ByteString.Builder
 import qualified Data.ByteString.Lazy.Char8 as BSLC
 import           Data.Functor.Identity
 import           Data.Text.Encoding
@@ -17,44 +18,33 @@ import           Network.Wai
 
 
 
-data HttpMetrics f =
-       HttpMetrics
-         { hmLatency :: No Identity f (Vector3 Histogram)
-         , hmStatus  :: No Identity f (Vector3 Counter)
-         }
-       deriving Generic
+newtype HttpMetrics f =
+          HttpMetrics
+            { hmDuration :: No Identity f (Vector3 Histogram)
+            }
+          deriving Generic
 
 {-# NOINLINE httpMetrics #-}
-httpMetrics :: BSLC.ByteString -> HttpMetrics Metric
-httpMetrics component =
-  HttpMetrics
-    { hmLatency = vector ("handler", "method", "status_code") $ histogram infoL buckets
-    , hmStatus  = vector ("handler", "method", "status_code") $ counter infoC
-    }
-  where
-    infoL = Info (component <> "_http_request_duration_milliseconds") "The HTTP request latencies in milliseconds."
-    infoC = Info (component <> "_http_status") "The HTTP respond status code count for each request."
-
-    buckets = [ 0, 0.2, 0.4, 0.5, 0.7, 1, 10, 50, 100, 200, 500, 1_000, 2_000, 5_000, 10_000 ]
+httpMetrics :: HttpMetrics Metric
+httpMetrics =
+  let buckets = take 14 . iterate (*2) $ 1 / 1024
+  in HttpMetrics
+       { hmDuration = vector ("path", "method", "status") $
+                        histogram (Info "http_req_duration" "HTTP request duration (in seconds)") buckets
+       }
 
 
 
-prometheus'
-  :: (Request -> BSLC.ByteString) -- ^ Endpoint path ("handler" label)
-  -> HttpMetrics Identity
+prometheus
+  :: HttpMetrics Identity
   -> Middleware
-prometheus' f metric app req respond = do
+prometheus metric app req respond = do
   start <- getMonotonicTimeNSec
   app req $ \res -> do
     end <- getMonotonicTimeNSec
-    let method  = BSLC.fromStrict $ requestMethod req
-        status  = BSLC.pack . show . statusCode $ responseStatus res
-        latency = fromIntegral (end - start) / 1_000_000
-    withLabel (f req, method, status) (hmLatency metric) $ observe latency
-    withLabel (f req, method, status) (hmStatus metric) increment
+    let path    = BSLC.fromStrict $ rawPathInfo req
+        method  = BSLC.fromStrict $ requestMethod req
+        status  = toLazyByteString . intDec . statusCode $ responseStatus res
+        latency = fromIntegral (end - start) / 1_000_000_000
+    withLabel (path, method, status) (hmDuration metric) $ observe latency
     respond res
-
-
-
-prometheus :: HttpMetrics Identity -> Middleware
-prometheus = prometheus' $ BSLC.fromStrict . rawPathInfo
